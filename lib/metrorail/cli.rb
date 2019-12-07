d31febd0f848a41a73954911122ddd94d776827e
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-12-07 16:37:53 +0000

    added option to menu for wrong answers via 'else'

--- lib/metrorail/cli.rb
+++ lib/metrorail/cli.rb
@@ -19,13 +19,17 @@ class Metrorail::CLI
 def menu 
  input = nil
   while input != "exit"
-   puts "Enter the number of the Issue you'd like more information on, type exit:"
+   puts "Enter the number of the Issue you'd like more information on or type list to see the issues again or exit:"
     input = gets.strip.downcase
     case input 
     when "1"
       puts "More info on Elevator Issues..."
     when "2"
       puts "More info on Escalator Issues..."
+    when "list"
+      list_issues
+    else 
+      puts "Not sure what you want, type list or exit"
       end
     end 
   end
