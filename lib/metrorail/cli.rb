11bd73ed280c668d55bacc8195b0c211e7d6c816
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-12-07 16:14:55 +0000

    added menu to cli.rb

--- lib/metrorail/cli.rb
+++ lib/metrorail/cli.rb
@@ -2,17 +2,21 @@
 class Metrorail::CLI 
   
   def call 
-    puts "Current Metrorail Issues:"
     list_issues
+    menu
   end
   
   def list_issues
     # maybe put a Here Doc here, if you can figure out how to make it work correctly, had problem
     puts #DOC
+    puts "Current Metrorail Issues:"
     puts "1. Elevator Issues"
     puts "2. Escalator Issues"
     # DOC 
-    
   end 
   
+def menu 
+  puts "Enter the number of the Issue You'd like more infor on:"
+end 
+
 end 
